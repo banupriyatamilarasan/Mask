ad806be9682de7f546f20451c5aa14e0884ad01b
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 22:18:03 +0530

    found

---
+++ src/test/java/org/menu/Nothing.java
@@ -0,0 +1,15 @@
+package org.menu;
+
+public class Nothing {
+	private void staff() {
+		System.out.println("r");
+	}
+	private void staff1() {
+		System.out.println("m");
+	}
+	private void staff2() {
+		System.out.println("t");
+	}
+	
+
+}
